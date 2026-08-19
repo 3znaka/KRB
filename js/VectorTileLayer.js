30988e2a4908602ef7baac0bd707ad3a13e94720
3znaka <mightygyvermac@gmail.com>
2026-08-19 13:34:18 +0300

    откат

--- js/VectorTileLayer.js
+++ js/VectorTileLayer.js
@@ -1096,7 +1096,14 @@ export class VectorTileLayer {
             const sx = (ndc.x * 0.5 + 0.5) * rect.width;
             const sy = (-ndc.y * 0.5 + 0.5) * rect.height;
 
-            if (ndc.z > 1 || ndc.z < -1) continue;
+            if (
+                sx < -cullMargin ||
+                sx > rect.width + cullMargin ||
+                sy < -cullMargin ||
+                sy > rect.height + cullMargin
+            ) {
+                continue;
+            }
 
             candidates.push({
                 pt,

9ba110e0d63f68e7da9cd7881b22165f5db18447
3znaka <108580297+3znaka@users.noreply.github.com>
2026-08-20 03:26:01 +0300

    октат

--- js/Marker3D.js
+++ js/Marker3D.js
@@ -340,7 +340,7 @@ export class Marker3D {
         if (this._modelPromise) return this._modelPromise;
         this._modelPromise = (async () => {
             try {
-                const gltfModule = await import('../js_TP/gltf.js');
+                const gltfModule = await import('https://esm.sh/three@0.160.0/examples/jsm/loaders/GLTFLoader.js');
                 const GLTFLoader = gltfModule.GLTFLoader;
                 const loader = new GLTFLoader();
                 const gltf = await loader.loadAsync(this._modelUrl);

822a6c9973318e26d2f745d3d6e835ee8fd349c1
3znaka <108580297+3znaka@users.noreply.github.com>
2026-08-26 23:02:31 +0300

    Update VectorTileLayer.js

--- js/VectorTileLayer.js
+++ js/VectorTileLayer.js
@@ -129,7 +129,7 @@ class VectorPointLabelSource {
  *     labelMaxPerTileClose: 20,
  *     labelCullMargin: 50,
  *     debug: false,
- *     styles: { building: { color: 'ff0000', stroke: 'black' } },
+ *     styles: { building: { color: 0xff0000, stroke: 'black' } },
  *     workerScripts: [
  *         'https://cdn.mapengine.ru/KRB/js_TP/tpb.js',
  *         'https://cdn.mapengine.ru/KRB/js_TP/earcut.js'

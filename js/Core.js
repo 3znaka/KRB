2e650c9becfd3384f4744ac930f6f86bbedb536f
3znaka <mightygyvermac@gmail.com>
2026-09-07 12:05:44 +0300

    Revert "новая попытка"
    This reverts commit f8a765d1ff33129e3570cf7002fdbbc68659ae15.

--- js/Core.js
+++ js/Core.js
@@ -1343,8 +1343,18 @@ _startCameraAnimationLoopIfNeeded() {
 
 this.maybeUpdateVisibleTiles();
 
-this.textManager.update();
-this.renderer.render(this.scene, this.camera);
+if (!this._pendingLabelUpdate) {
+    // Обновляем подписи и принудительно применяем layout
+    this.textManager.update();
+    void this.textManager.pane.offsetWidth;
+
+    // Откладываем рендер canvas на следующий кадр
+    this._pendingLabelUpdate = true;
+    requestAnimationFrame(() => {
+        this.renderer.render(this.scene, this.camera);
+        this._pendingLabelUpdate = false;
+    });
+}
 
         for (const layer of this._dynamicLayers) {
             if (layer._postUpdate) layer._postUpdate(this);
